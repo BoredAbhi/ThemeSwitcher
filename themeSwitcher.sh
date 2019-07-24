cde10df9c20edecb618b660fcbbf403410cb9331
Abhijeet Bhattacharjee <teejihba8@gmail.com>
2019-07-24 10:15:39 +0530

    Updating Night Wallapaer

--- themeSwitcher.sh
+++ themeSwitcher.sh
@@ -3,7 +3,7 @@
 echo 'Starting Theme Switcher'
 
 DAY_WALLPAPER=~/Pictures/MXDarkCity16x10Blue.png
-NIGHT_WALLPAPER=~/Pictures/MXDarkCity16x10.png
+NIGHT_WALLPAPER=/usr/share/backgrounds/MX-Gold1080P.png
 HOUR="$(date | cut -d ' ' -f 4 | cut -d ':' -f 1)"
 
 #If time is past 6:59 PM but before 5:59 AM 
@@ -12,6 +12,7 @@ then
 	echo "Setting Night Theme"
 	xfconf-query -c xsettings -p /Net/ThemeName -s "Arc-Dark"
 	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s $NIGHT_WALLPAPER
+
 #Else Light Theme 
 else
 	echo "Setting Day Theme"
