189d583086d79c389f78085d9f4347a28183c291
Abhijeet Bhattacharjee <teejihba8@gmail.com>
2019-08-12 23:58:10 +0530

    Update themeSwitcher.sh

--- themeSwitcher.sh
+++ themeSwitcher.sh
@@ -2,7 +2,7 @@
 
 #INFO : Run script at startup and a cron at 6:00AM and 7:00PM.
 
-HOUR="$(date | cut -d ' ' -f 4 | cut -d ':' -f 1)"
+HOUR="$(date | cut -d ' ' -f 4 | cut -d ':' -f 1)" #Note: Earlier -f 4 worked perfectly until Aug 1
 
 #If Night : time is past 6:59 PM but before 5:59 AM 
 if [ $HOUR -gt 18 ] || [ $HOUR -lt 6 ]
@@ -13,7 +13,8 @@ then
 	NIGHT_WALLPAPER_PATH="$NIGHT_WALLPAPER_DIR$NIGHT_WALLPAPER"
 	xfconf-query -c xsettings -p /Net/ThemeName -s "Arc-Dark"
 	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s $NIGHT_WALLPAPER_PATH
-	sh "/home/abhimx/.conky/conky-startup.sh"
+	#wal -i $NIGHT_WALLPAPER_PATH
+	#sh "/home/abhimx/.conky/conky-startup.sh"
 
 #Else Day : Light Theme 
 else
@@ -23,4 +24,9 @@ else
 	DAY_WALLPAPER_PATH="$DAY_WALLPAPER_DIR$DAY_WALLPAPER"
 	xfconf-query -c xsettings -p /Net/ThemeName -s "Numix"
 	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s $DAY_WALLPAPER_PATH
+	#wal -i $DAY_WALLPAPER_PATH
+	#sh "/home/abhimx/.conky/conky-startup.sh"
 fi
+
+#Execute Terminal Theme
+./terminal_bg_switcher.sh
